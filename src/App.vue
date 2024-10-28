7a73b3579b9f9428f7452421a32eea37eae8aba2
veloz-web <adelorie@silveredge-gs.com>
2024-10-28 08:34:51 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <NFLNavigation />
+    <v-main tabindex="-1">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import NFLNavigation from './components/NFLNavigation.vue'
+</script>
